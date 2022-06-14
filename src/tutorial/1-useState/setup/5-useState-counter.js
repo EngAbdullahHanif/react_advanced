97d3bdd91bf965c1613630d05627b794e87aff58
engAbdullahHaif <engabdullahhanif@gmail.com>
2022-06-14 09:54:04 +0430

    adding functional useState

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,12 +2,26 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   const [num, setNum] = useState(0)
-  return <>
-    <h1>{num}</h1>
-    <button className='btn' onClick={() => {setNum(num - 1)}}>Decrease</button>
-    <button className='btn' onClick={() => {setNum(0)}}>Reset</button>
-    <button className='btn' onClick={() => {setNum(num + 1)}}>Increase</button>
-  
+  const ComplexCounter = () => {
+    setTimeout(() => {
+      setNum((preNum) => {
+        return preNum + 1
+      })
+    }, 2000)
+  }
+  return <> 
+  ‍  <section>
+      <h1>Regualr Counter</h1>
+      <h1>{num}</h1>
+      <button className='btn' onClick={() => {setNum(num - 1)}}>Decrease</button>
+      <button className='btn' onClick={() => {setNum(0)}}>Reset</button>
+      <button className='btn' onClick={() => {setNum(num + 1)}}>Increase</button>
+    </section>
+    <section>
+      <h1>Complex Counter</h1>
+      <h1>{num}</h1>
+      <button className='btn' onClick={ComplexCounter}>Increase</button>
+    </section>
   </>;
 };
 
