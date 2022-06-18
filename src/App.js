31e57abc13a2d96893837a15870a406474595d5d
engAbdullahHaif <engabdullahhanif@gmail.com>
2022-06-18 15:07:11 +0430

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Setup from './tutorial/1-useState/setup/2-useState-basics'
 import UseStateCounter from './tutorial/1-useState/setup/5-useState-counter'
 import Cleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
 import FetchData from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+import Multiple from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
 
 function App() {
   return (
     <div className='container'>
-      <FetchData />
+      <Multiple />
     </div>
   )
 }
